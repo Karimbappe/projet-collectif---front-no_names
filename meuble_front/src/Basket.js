11983cde41475753bc058ce80b88972929aae61b
Marielle K <marielle.koffi1@gmail.com>
2022-11-09 11:36:55 +0100

    Add Basket.js and react-Redux

---
+++ meuble_front/src/Basket.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Basket() {
+  const [nbProducts, setnbProducts] = useState(1);
+  const updateNbProduct = (e) => {
+    setnbProducts(Number(e.target.value));
+  };
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="quantity">Quantité</label>
+        <input
+          type="number"
+          id="quantity"
+          value={nbProducts}
+          onChange={updateNbProduct}
+        ></input>
+        <button>Ajouter au panier</button>
+        <span className="adding-info"></span>
+      </form>
+    </div>
+  );
+}
+
+export default Basket;
--- meuble_front/src/Product.js
+++ meuble_front/src/Product.js
@@ -1,71 +1,64 @@
 //import React from 'react'
-import Navbar from './Navbar'
-import Footer from './Footer'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import React, { useEffect, useState } from "react"
-import {useParams} from 'react-router-dom'
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 //import StripeCheckout from 'react-stripe-checkout'
 
-
 function Product() {
-    const paramsUrl = useParams();
+  const paramsUrl = useParams();
 
-    console.log(paramsUrl._id); 
+  console.log(paramsUrl._id);
 
-    const [Product, setProduct] = useState([])
-    
-    const fetchData = () => {
-        fetch("http://localhost:8000/api/products/find/" + paramsUrl._id)
-        .then(response => {
-            let reponseData = response.json()
-            return reponseData
-        })
-        .then(data => {
-            setProduct(data)
-            console.log(data)
-        })
-    }
-    
-    useEffect(() => {
-        fetchData()
-    }, [])
+  const [Product, setProduct] = useState([]);
 
+  const fetchData = () => {
+    fetch("http://localhost:8000/api/products/find/" + paramsUrl._id)
+      .then((response) => {
+        let reponseData = response.json();
+        return reponseData;
+      })
+      .then((data) => {
+        setProduct(data);
+        console.log(data);
+      });
+  };
 
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    //return <h2>userId is 👉️ {params.userId}</h2>;
+  //return <h2>userId is 👉️ {params.userId}</h2>;
 
-    return (
+  return (
     <div>
-        <Navbar />
-        <div className='cards_container card_product' >
-            <div>
-                <div className='cards_content'>
-                    <Card  href="/#/Product">
-                    <Card.Link href={"/#/Product/" +  Product._id} ><Card.Img className='imgcart' variant="top" src={Product.image} /></Card.Link>
-                    <Card.Body>
-                    <Card.Title>{Product.name}</Card.Title>
-                    <Card.Text>
-                    {Product.description}
-                    </Card.Text>
-                    <Card.Text>
-                    {Product.price} € 
-                    </Card.Text>
-                    </Card.Body>
-                    </Card>
-                </div>
-            </div>
+      <Navbar />
+      <div className="cards_container card_product">
+        <div>
+          <div className="cards_content">
+            <Card href="/#/Product">
+              <Card.Link href={"/#/Product/" + Product._id}>
+                <Card.Img
+                  className="imgcart"
+                  variant="top"
+                  src={Product.image}
+                />
+              </Card.Link>
+              <Card.Body>
+                <Card.Title>{Product.name}</Card.Title>
+                <Card.Text>{Product.description}</Card.Text>
+                <Card.Text>{Product.price} €</Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
         </div>
+      </div>
 
-
-        
-        <Footer />  
+      <Footer />
     </div>
-    )
-
-
+  );
 }
 
-
-export default Product
-
+export default Product;
